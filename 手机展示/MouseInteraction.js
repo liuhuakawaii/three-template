2f3911b3fb7b1b990082b75c75e938ed149f3c3c
ergo <liuhuakawaii@163.com>
2024-08-19 18:23:47 +0800

    Update: 手机展示案例

---
+++ 手机展示/MouseInteraction.js
@@ -0,0 +1,124 @@
+import * as THREE from 'three';
+
+export default class MouseInteraction {
+  constructor(baseScene) {
+    this.baseScene = baseScene;
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+    this.selectedObject = null;
+    this.hoverObject = null;
+    this.intersects = [];
+
+    this.onClickCallback = null;
+    this.onDblClickCallback = null;
+    this.onHoverCallback = null;
+
+    this.lastClickTime = 0;
+    this.doubleClickTimeout = 300; // 双击的最长时间间隔（毫秒）
+
+    this.init();
+  }
+
+  init() {
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    const canvas = this.baseScene.renderer.domElement;
+    canvas.addEventListener('mousemove', this.onMouseMove.bind(this), false); //false 表示事件处理程序不会阻止事件的默认行为
+    canvas.addEventListener('click', this.onClick.bind(this), false);
+    canvas.addEventListener('dblclick', this.onDblClick.bind(this), false);
+  }
+
+  onMouseMove(event) {
+    event.preventDefault();
+    const canvas = this.baseScene.renderer.domElement;
+    const rect = canvas.getBoundingClientRect();
+    this.mouse.x = ((event.clientX - rect.left) / canvas.clientWidth) * 2 - 1;
+    this.mouse.y = -((event.clientY - rect.top) / canvas.clientHeight) * 2 + 1;  //坐标系转化
+
+    this.checkIntersection();
+
+    if (this.hoverObject !== this.intersects[0]?.object) {
+      if (this.hoverObject) this.onMouseLeave(this.hoverObject);
+      this.hoverObject = this.intersects[0]?.object;
+      if (this.hoverObject) this.onMouseEnter(this.hoverObject);
+    }
+
+    if (this.onHoverCallback) this.onHoverCallback(this.hoverObject);
+  }
+
+  onClick(event) {
+    event.preventDefault();
+    const currentTime = Date.now();
+    if (currentTime - this.lastClickTime < this.doubleClickTimeout) {
+      // 如果在短时间内发生两次点击，则认为是双击
+      return;
+    }
+
+    this.lastClickTime = currentTime;
+    this.checkIntersection(false);
+    if (this.intersects.length > 0) {
+      this.selectedObject = this.intersects[0].object;
+      if (this.onClickCallback) this.onClickCallback(this.selectedObject);
+    } else {
+      this.selectedObject = null;
+      if (this.onClickCallback) this.onClickCallback(null);
+    }
+  }
+
+  onDblClick(event) {
+    event.preventDefault();
+    this.checkIntersection();
+
+    if (this.intersects.length > 0) {
+      const dblClickedObject = this.intersects[0].object;
+      if (this.onDblClickCallback) this.onDblClickCallback(dblClickedObject);
+    } else {
+      if (this.onDblClickCallback) this.onDblClickCallback(null);
+    }
+  }
+
+  checkIntersection(recursion = true) {
+    //通过鼠标位置和相机计算射线 设置射线的起点和方向
+    this.raycaster.setFromCamera(this.mouse, this.baseScene.camera);
+    // 第二个参数为 true 表示递归检查子对象
+    this.intersects = this.raycaster.intersectObjects(this.baseScene.scene.children, recursion);
+  }
+
+  onMouseEnter(object) {
+    if (object.material) {
+      // object.material.emissive = new THREE.Color(0x555555);
+    }
+  }
+
+  onMouseLeave(object) {
+    // 可以在这里添加鼠标离开对象时的效果，比如恢复原来的颜色
+    if (object.material) {
+      // object.material.emissive = new THREE.Color(0x000000);
+    }
+  }
+
+  setOnClickCallback(callback) {
+    this.onClickCallback = callback;
+  }
+
+  setOnDblClickCallback(callback) {
+    this.onDblClickCallback = callback;
+  }
+
+  setOnHoverCallback(callback) {
+    this.onHoverCallback = callback;
+  }
+
+  update() {
+    // 在这里可以添加需要在每一帧更新的逻辑
+  }
+
+  destroy() {
+    const canvas = this.baseScene.renderer.domElement;
+    canvas.removeEventListener('mousemove', this.onMouseMove);
+    canvas.removeEventListener('click', this.onClick);
+    canvas.removeEventListener('dblclick', this.onDblClick);
+  }
+}
---
+++ 手机展示/baseScene.js
@@ -0,0 +1,197 @@
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+import { ColladaLoader } from 'three/addons/loaders/ColladaLoader.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { PLYLoader } from 'three/addons/loaders/PLYLoader.js';
+import { STLLoader } from 'three/addons/loaders/STLLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+export default class BaseScene {
+  constructor(container) {
+    this.container = container;
+    this.scene = new THREE.Scene();
+    this.camera = null;
+    this.renderer = null;
+    this.controls = null;
+    this.fileLoaderMap = {
+      'glb': new GLTFLoader(),
+      'fbx': new FBXLoader(),
+      'gltf': new GLTFLoader(),
+      'obj': new OBJLoader(),
+      'dae': new ColladaLoader(),
+      'stl': new STLLoader(),
+      'ply': new PLYLoader(),
+    }
+    this.animation = null
+    this.textureLoader = new THREE.TextureLoader();
+    this.sequenceAnimations = [];
+
+    this.init();
+  }
+
+  init() {
+    this.createCamera();
+    // this.createOrthographicCamera()
+    this.createRenderer();
+    this.createControls();
+    this.createLights();
+    this.addEventListeners();
+    // this.createFog(0x005577, -100, 1000);
+  }
+
+  createCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+      30,
+      this.container.clientWidth / this.container.clientHeight,
+      0.1,
+      3000);
+    this.camera.position.set(292, 223, 185);
+  }
+
+  createOrthographicCamera() {
+    this.camera = new THREE.OrthographicCamera(this.container.clientWidth / - 2, this.container.clientWidth / 2, this.container.clientHeight / 2, this.container.clientHeight / - 2, 0.1, 3000);
+    this.camera.position.set(292, 223, 185);
+  }
+
+  createRenderer() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setClearColor(0x000000, 1);
+    this.container.appendChild(this.renderer.domElement);
+
+    this.renderer.outputEncoding = THREE.sRGBEncoding; //解决模型颜色变暗的问题
+  }
+
+  createControls() {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+  }
+
+  createLights() {
+    // 平行光1
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(400, 200, 300);
+    this.scene.add(directionalLight);
+    // 平行光2
+    var directionalLight2 = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight2.position.set(-400, -200, -300);
+    this.scene.add(directionalLight2);
+    //环境光
+    var ambient = new THREE.AmbientLight(0xffffff, 1.9);
+    this.scene.add(ambient);
+  }
+
+  createFog(color, near, far) {
+    this.scene.fog = new THREE.Fog(color, near, far);
+  }
+
+  addFlame(pos) {
+    const flame = createFlame()
+    this.scene.add(flame)
+    flame.position.copy(pos)
+    flame.position.y += 10;
+  }
+
+  addEventListeners() {
+    window.addEventListener('resize', this.onWindowResize.bind(this), false);
+  }
+
+  onWindowResize() {
+    this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+  }
+
+  render() {
+    requestAnimationFrame(this.render.bind(this));
+    this.controls.update();
+    // this.animation.UpdateLoop()
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  addMesh(mesh) {
+    this.scene.add(mesh);
+  }
+
+  loadModelByUpload(file) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const extension = file.name.split('.').pop().toLowerCase();
+      if (this.fileLoaderMap[extension]) {
+        this.fileLoaderMap[extension].load(event.target.result, (model) => {
+          this.clearModel()
+          let tempScene
+          if (extension === 'glb') {
+            tempScene = model.scene;
+          } else {
+            tempScene = model;
+          }
+          // const boundingBox = new THREE.Box3().setFromObject(tempScene);
+          // const center = new THREE.Vector3();
+          // boundingBox.getCenter(center); // 计算 mesh 的中心点并将其移动到原点
+          // const meshGroup = new THREE.Group();
+          // // 遍历 this.originMesh 的子对象，将所有子对象的位置调整以使中心点在原点
+
+          // tempScene.traverse((object) => {
+          //   if (object.isMesh) {
+          //     object.position.sub(center);
+          //     meshGroup.add(object.clone());
+          //   }
+          // });
+          this.originMesh = tempScene
+
+          this.scene.add(this.originMesh);
+        });
+      } else {
+        console.log('不支持该文件类型')
+      }
+    }
+    reader.readAsDataURL(file);
+  }
+
+  loadModelByPath(url, cb) {
+    const extension = url.split('.').pop().toLowerCase();
+    if (this.fileLoaderMap[extension]) {
+      fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+          const reader = new FileReader();
+          reader.onload = (event) => {
+            this.fileLoaderMap[extension].load(event.target.result, (model) => {
+
+              let tempScene;
+              if (['glb', 'gltf'].includes(extension)) {
+                tempScene = model.scene;
+              } else {
+                tempScene = model;
+              }
+              // 以下是你原有的逻辑
+              // ...
+              this.originMesh = tempScene;
+              this.scene.add(this.originMesh);
+              cb && cb()
+            });
+          };
+          reader.readAsDataURL(blob);
+        })
+        .catch(error => {
+          console.error('加载模型失败:', error);
+        });
+    } else {
+      console.log('不支持该文件类型');
+    }
+  }
+
+  traverseScene(ui2D) {
+    const group = this.scene.getObjectByName('粮仓')
+    group.traverse((object) => {
+      if (object.isMesh) {
+        const pos = object.getWorldPosition(new THREE.Vector3())
+        ui2D.createLabel(object.name, pos)
+      }
+    });
+  }
+
+}
